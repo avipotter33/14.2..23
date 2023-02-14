770eeda22e56454467ae9c5fc6d94b9c1297e2b8
Aiiiiiiii <avia111206@gmail.com>
2023-02-14 18:47:38 +0200

    WOW :0

--- Avia wow mekori.py
+++ Avia wow mekori.py
@@ -1,2 +1,3 @@
 def print_funny_story():
-    print("When I two years old I ate pancakes with ketchup. I liked it.")
+    print("When I was two years old I ate pancakes with ketchup. I liked it.")
+    print("sdfhgjkl;'adsjkjkjkdas")
